4dc8a7ed4d27f20db75427f00187a39cb33c7c75
SeanMa <335254110@qq.com>
2018-01-04 22:05:46 +0800

    隐藏actionbar

--- app/src/main/java/www/sean/com/bitmapcompress/MainActivity.java
+++ app/src/main/java/www/sean/com/bitmapcompress/MainActivity.java
@@ -1,7 +1,10 @@
 package www.sean.com.bitmapcompress;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //隐藏状态栏
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 }
