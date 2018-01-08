e118f46727a037222e479fb8a7c70f7e9a7cf54d
SeanMa <335254110@qq.com>
2018-01-08 22:46:15 +0800

    采样率压缩

--- app/src/main/java/www/sean/com/bitmapcompress/MainActivity.java
+++ app/src/main/java/www/sean/com/bitmapcompress/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import www.sean.com.bitmapcompress.fragment.FragmentA;
+import www.sean.com.bitmapcompress.fragment.FragmentB;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = findViewById(R.id.viewpager);
         List<Fragment> list = new ArrayList<>();
         list.add(new FragmentA());
+        list.add(new FragmentB());
         FragAdapter adapter = new FragAdapter(list,getSupportFragmentManager());
 
         mViewPager.setAdapter(adapter);
---
+++ app/src/main/java/www/sean/com/bitmapcompress/fragment/FragmentB.java
@@ -0,0 +1,46 @@
+package www.sean.com.bitmapcompress.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import www.sean.com.bitmapcompress.R;
+import www.sean.com.bitmapcompress.view.MyImageView;
+
+import static www.sean.com.bitmapcompress.Constants.COMPRESS_QUALITY;
+import static www.sean.com.bitmapcompress.Constants.COMPRESS_SAMPLING;
+
+/**
+ * Created by SeanMa on 2018/1/8.
+ */
+
+public class FragmentB extends Fragment implements MyImageView.TextListener{
+
+    private TextView tvSize;
+    private TextView tvSrcSize;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_page,container,false);
+        TextView text = view.findViewById(R.id.text);
+        text.setText("采样率压缩");
+        MyImageView imageView = view.findViewById(R.id.imageview);
+        imageView.setListener(this);
+        imageView.setCompressType(COMPRESS_SAMPLING);
+        tvSize = view.findViewById(R.id.size);
+        tvSrcSize = view.findViewById(R.id.src_size);
+
+        return view;
+    }
+
+    @Override
+    public void showText(String srcSize, String size) {
+        tvSize.setText(size);
+        tvSrcSize.setText(srcSize);
+    }
+}
